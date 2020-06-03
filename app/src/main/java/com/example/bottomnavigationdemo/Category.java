2e7d287662e197879d4e8e35050969c4b5fa7bde
Wei Man <hah874100@gmail.com>
2020-06-03 15:09:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationdemo/Category.java
@@ -0,0 +1,50 @@
+package com.example.bottomnavigationdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        BottomNavigationView bottomNavigationView  = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.category);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.cart:
+                        startActivity(new Intent(getApplicationContext(),Cart.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.logo:
+                        startActivity(new Intent(getApplicationContext(),Logo.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.category:
+                        return true;
+                    case R.id.user:
+                        startActivity(new Intent(getApplicationContext(),User.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/bottomnavigationdemo/Logo.java
@@ -0,0 +1,50 @@
+package com.example.bottomnavigationdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Logo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+
+        BottomNavigationView bottomNavigationView  = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.logo);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.category:
+                        startActivity(new Intent(getApplicationContext(),Category.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.logo:
+                        return true;
+                    case R.id.cart:
+                        startActivity(new Intent(getApplicationContext(),Cart.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.user:
+                        startActivity(new Intent(getApplicationContext(),User.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
